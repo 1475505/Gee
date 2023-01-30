90481e69280757dc7754fe264dcf44c38f27d436
1475505 <liul2566@bupt.edu.cn>
2023-01-31 02:21:29 +0800

    RPC客户端

--- gee-rpc/server.go
+++ gee-rpc/server.go
@@ -135,7 +135,7 @@ func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interfa
 }
 
 func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
-	// TODO, should call registered rpc methods to get the right replyv. just print argv
+	// TODO: should call registered rpc methods to get the right replyv. just print argv
 	defer wg.Done()
 	log.Println(req.h, req.argv.Elem())
 	req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq))
